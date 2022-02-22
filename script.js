a0da8a30cb05d022c242358ef47e1b7d9dfac18a
Seregey_Maximov <dev9092336062@gmail.com>
2022-02-22 11:07:03 +0300

    script upload file: name

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const textInput = document.getElementById('textInput')
 const fileNameInput = document.getElementById('fileNameInput')
 const txtSaveButton = document.getElementById('saveBtnTxt')
+const fileInput = document.getElementById('fileUpload');
+const fileUploadLabel = document.getElementById('fileUploadLabel');
 
 const fileValue = {
     value: '',
@@ -16,6 +18,8 @@ const fileName = {
     }
 }
 
+fileInput.addEventListener('change', updateImageDisplay);
+
 txtSaveButton.download = `${fileName.name}.txt`
 
 textInput.oninput = function () {
@@ -27,6 +31,15 @@ fileNameInput.oninput = function () {
     fileName.setName(fileNameInput.value);
     fileNameInput.innerHTML = fileName.name
     txtSaveButton.download = `${fileName.name}.txt`
+    txtSaveButton.innerHTML = `Download as "${fileName.name}.txt"`
+}
+
+function updateImageDisplay(e) {
+    const file = fileInput.files[0]
+    const name = file.name.slice(0, file.name.length - 4)
+    fileName.setName(name)
+    fileUploadLabel.innerHTML = `${fileName.name}.txt`
+    fileNameInput.value = fileName.name
     txtSaveButton.innerHTML = `Download as "${fileName.name}.txt"`
 }
 
